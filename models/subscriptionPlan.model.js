e05b550ba3b3e6c0577a76eedb971596f27484c7
mobasshir <mobasshirkhan9931@gmail.com>
2025-09-02 16:00:11 +0530

    subscription model defined

---
+++ models/subscriptionPlan.model.js
@@ -0,0 +1,27 @@
+
+
+const SubscriptionPlanSchema = new mongoose.Schema({
+    planName: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    duration: {
+        type: String,
+        required: true,
+    },
+    subscribers: {
+        type: Number,
+        default: 0,
+    },
+    Status:{
+        type: Boolean,
+        default: Inactive,
+    }
+})
+
+const subscriptionPlan = mongoose.model('subscriptionPlan', SubscriptionPlanSchema)
+module.exports = subscriptionPlan
