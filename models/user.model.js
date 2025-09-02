24e125d3d76e5b56af2056925b5684c5d8e91d04
mobasshir <mobasshirkhan9931@gmail.com>
2025-09-02 16:00:35 +0530

    user schema defined

---
+++ models/user.model.js
@@ -0,0 +1,51 @@
+const subscriptionPlan = require("./subscriptionPlan.model");
+
+
+const  UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    adharNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    subscriptionPlan: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubscriptionPlan",
+        required: true,
+    },
+    joiningDate: {
+        type: Date,
+        default: Date.now,
+    },
+    feePaid: {
+        type: Number,
+        required: true,
+        default: false,
+    },
+    seatNumeber: {
+        type: Number,
+        required: true,
+    },
+    age: {
+        type: Number,
+    },
+    address: {
+        type: String,
+    },
+    idNumber: {
+        type: Number,
+        required: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: false,
+    },
+
+
+})
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
