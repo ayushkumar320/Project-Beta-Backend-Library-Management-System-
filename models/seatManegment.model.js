d2cedfb4abe0e63f991d2416f9b6972bc572601e
mobasshir <mobasshirkhan9931@gmail.com>
2025-09-02 18:06:33 +0530

    seat managment sechma

---
+++ models/seatManegment.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const SeatManegmentSchema = new mongoose.Schema({
+    seatNumber: {
+        type: String,
+        required: true,
+    },
+    student:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    plan:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubscriptionPlan",
+        required: true,
+    },
+    allocationDate: {
+        type: Date,
+        default: Date.now,
+    },
+    expirationDate: {
+        type: Date,
+        required: true,
+    },
+    status: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+export default mongoose.model("SeatManegment", SeatManegmentSchema);
