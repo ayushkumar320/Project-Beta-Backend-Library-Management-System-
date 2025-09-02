4abb2cfb5b0b062a51b2b465ab12816a90c94b65
mobasshir <mobasshirkhan9931@gmail.com>
2025-09-02 16:14:35 +0530

    added jwt authorization for admin

---
+++ middlewares/admin.auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import Admin from '../models/admin.model.js';
+
+const adminAuth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('process.env.JWT_SECRET ', '');
+        if (!token) {
+            return res.status(401).json({ message: 'Authentication token is missing' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'JWT_SECRET');
+        const admin = await Admin.findOne({ _id: decoded._id });
+
+        if (!admin) {
+            return res.status(401).json({ message: 'Admin not found' });
+        }
+
+        req.token = token;
+        req.admin = admin;
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Please authenticate' });
+    }
+};
+
+export default adminAuth;
