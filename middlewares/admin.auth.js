0bc166d9974c61bcce9bcb0d12f3e064d695f2fa
Ayush <ayushkumar85385@gmail.com>
2025-09-02 16:29:04 +0530

    fixed the auth middleware

--- middlewares/admin.auth.js
+++ middlewares/admin.auth.js
@@ -1,26 +1,27 @@
-import jwt from 'jsonwebtoken';
-import Admin from '../models/admin.model.js';
+import jwt from "jsonwebtoken";
+import Admin from "../models/admin.model.js";
 
 const adminAuth = async (req, res, next) => {
-    try {
-        const token = req.header('Authorization').split(" ")[1];
-        if (!token) {
-            return res.status(401).json({ message: 'Authentication token is missing' });
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'JWT_SECRET');
-        const admin = await Admin.findOne({ _id: decoded._id });
-
-        if (!admin) {
-            return res.status(401).json({ message: 'Admin not found' });
-        }
-
-        req.token = token;
-        req.admin = admin;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Please authenticate' });
+  try {
+    // Fixed the bearer token extraction
+    const token = req.header("Authorization").split(" ")[1];
+    if (!token) {
+      return res.status(401).json({message: "Authentication token is missing"});
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const admin = await Admin.findOne({_id: decoded._id});
+
+    if (!admin) {
+      return res.status(401).json({message: "Admin not found"});
+    }
+
+    req.token = token;
+    req.admin = admin;
+    next();
+  } catch (error) {
+    res.status(401).json({message: "Please authenticate"});
+  }
 };
 
 export default adminAuth;
