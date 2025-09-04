4c27e542aafbcfec04949a6ee1bad854227b72c0
Ayush <ayushkumar85385@gmail.com>
2025-09-04 16:33:39 +0530

    added / route

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/admin", router);
 
+app.get("/", (req, res) => {
+  res.send("Server is healthy!");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
